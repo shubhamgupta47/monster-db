918b4c2cc51ff1113bd4b9cce5f7f1fd58500663
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-10 15:11:08 +0530

    Updates

---
+++ precache-manifest.3426e5445c476ed76b48697c56560bb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee67ac09444cceda693dc44dead9565",
+    "url": "/monster-db/index.html"
+  },
+  {
+    "revision": "fe432be677a5dc9fe557",
+    "url": "/monster-db/static/css/main.581704b0.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monster-db/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "fe432be677a5dc9fe557",
+    "url": "/monster-db/static/js/main.09dd3270.chunk.js"
+  },
+  {
+    "revision": "2cf197b6208da6e46e0a",
+    "url": "/monster-db/static/js/runtime~main.456e2295.js"
+  }
+]);
